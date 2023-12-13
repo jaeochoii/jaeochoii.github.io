495cd0bf075b6db3a0766962ec19b3e39e3e1687
jaeochoii <jaeochoiii0716@inha.edu>
2023-12-13 21:11:52 +0900

    Updates

---
+++ component---src-pages-404-js-3b5896b19bbfe8ab0829.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkzoomkoding_com=self.webpackChunkzoomkoding_com||[]).push([[883],{45:function(e,t,n){n.r(t);var o=n(7294),u=n(7776),l=n(3432);t.default=function(){return o.createElement(u.Z,null,o.createElement(l.Z,{title:"404: Not found"}),o.createElement("h1",null,"404: Not Found"),o.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-3b5896b19bbfe8ab0829.js.map
--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7c80ee6af0317049d955.js"
+    "url": "webpack-runtime-a80c20b07aa3f94b5b11.js"
   },
   {
     "url": "styles.43a1709a44aec0c5381e.css"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "framework-de157c6b30d4790e4e81.js"
   },
   {
-    "url": "app-54c04ed36349c825d230.js"
+    "url": "app-3292d18dc0d13294ce49.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ccc1679df9342b9b9231b208211cacce"
+    "revision": "7d35cd55c83a8e4862784f1c5614bd66"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -166,7 +166,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-54c04ed36349c825d230.js`))) {
+  if (!resources || !(await caches.match(`/app-3292d18dc0d13294ce49.js`))) {
     return await fetch(event.request)
   }
 
